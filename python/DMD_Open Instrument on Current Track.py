64f18601aad14e85b238625ede9a9f0a1d25c0f8
Dillon M. DeRosa <53786232+DMDComposer@users.noreply.github.com>
2022-04-07 14:00:04 -0400

    Update DMD_Open Instrument on Current Track.py

--- python/DMD_Open Instrument on Current Track.py
+++ python/DMD_Open Instrument on Current Track.py
@@ -1,8 +1,9 @@
 # @description Open Instrument on Current Track
 # @author DMDComposer
-# @version 1.0
+# @version 1.1
 # @about
 #   open first instrument on selected track
+#   toggle if already opened
 # reapy -- https://python-reapy.readthedocs.io/en/latest/api_guide.html#://
 
 from reapy import print, inside_reaper
@@ -13,9 +14,14 @@ import reapy.reascript_api as RPR
 def main():
     currTrack = RPR.GetSelectedTrack(0, 0)
     instrument = RPR.TrackFX_GetInstrument(currTrack)
+    isOpened = RPR.TrackFX_GetOpen(currTrack, instrument)
 
     if instrument >= 0:
-        RPR.TrackFX_Show(currTrack, instrument, 3)
+        if isOpened == True:
+            RPR.TrackFX_Show(currTrack, instrument, 2)
+        else:
+            RPR.TrackFX_Show(currTrack, instrument, 3)
+        return
 
     return
 
