e063702e80742acb0faa6a024879cb7a6bbcca6d
Dillon M. DeRosa <53786232+DMDComposer@users.noreply.github.com>
2022-04-13 20:08:56 -0400

    must've copied pasted wrong code, oops

--- MIDI Editor/DMD_Toggle Solo on Selected Items.py
+++ MIDI Editor/DMD_Toggle Solo on Selected Items.py
@@ -1,6 +1,6 @@
 # @description Toggle Solo on Selected Items within the Midi Editor
 # @author DMDComposer
-# @version 1.1
+# @version 1.2
 # @about
 #   Toggle solo on selected items within the Midi editor.
 # reapy -- https://python-reapy.readthedocs.io/en/latest/api_guide.html#://
@@ -13,10 +13,9 @@ import reapy.reascript_api as RPR
 def main():
     RPR.Undo_BeginBlock()
 
-    RPR.MIDIEditor_OnCommand(RPR.MIDIEditor_GetActive(), 2)
-    RPR.Main_OnCommand(41560, 0)
+    RPR.Main_OnCommand(41559, 0)
 
-    RPR.Undo_EndBlock("Close Midi Editor & Unsolo Items", 1)
+    RPR.Undo_EndBlock("Toggle Solo on Selected Items within the Midi Editor", 1)
     return
 
 
